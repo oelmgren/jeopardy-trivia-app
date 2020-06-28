b85adf1dde21f96ce3e7bb191a5e6aedfd4a85c6
Andrew Elmgren <oelmgren@umich.edu>
2020-06-28 14:11:00 -0700

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Ollie Elmgren on 6/28/20.
+//  Copyright © 2020 Ollie Elmgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
